b336729a32b52f02928f16d3e867371b2d554953
Lauren Shrack <laurenshrack@gmail.com>
2024-05-15 15:25:39 -0400

    added docstring for audio_to_midi

--- V0/audio_to_midi.py
+++ V0/audio_to_midi.py
@@ -3,6 +3,12 @@ from basic_pitch import ICASSP_2022_MODEL_PATH
 
 
 def audio_to_midi(audio_file, output_folder):
+    """
+    Run the basic-pitch model on an audio file.
+
+    audio_file: path to WAV file (or other file format compatible with basic-pitch)
+    output_folder: path to folder where outputs should be saved
+    """
     output_path = output_folder + audio_file.split('/')[-1].split('.')[0] + '_basic_pitch.mid'
     predict_and_save([audio_file],
                     output_folder,
